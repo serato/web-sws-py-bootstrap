dfa20a735d340e47db993b6105bd99df4e7fb2bb
Andrii Osmak <andrii.osmak@serato.com>
2021-06-23 16:25:05 +1200

    minor fix in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 setuptools.setup(
   name = 'web-sws-py-bootstrap',
-  packages = ['web-sws-py-bootstrap'],
+  packages = ['web_sws_py_bootstrap'],
   version = '0.1',
   license='MIT',
   description = 'Here we store all the common functionality that we use across our services.',
